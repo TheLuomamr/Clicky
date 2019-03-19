78afe55e7ca39cf8e8fa65b701149127d01827c4
Mark Luoma <luomamr@gmail.com>
2019-03-19 10:28:55 -0400

    Updates

---
+++ precache-manifest.5279169a4b5f205c4507003b37546404.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1726ed0b9997b87a72d7",
+    "url": "/Clicky/static/js/runtime~main.a36ce3a7.js"
+  },
+  {
+    "revision": "8bd9a652c9f3e12c4062",
+    "url": "/Clicky/static/js/main.9abb7bc5.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/Clicky/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "8bd9a652c9f3e12c4062",
+    "url": "/Clicky/static/css/main.ba5e39d0.chunk.css"
+  },
+  {
+    "revision": "94bdabc9abefc1f34213609fe682c470",
+    "url": "/Clicky/index.html"
+  }
+];
